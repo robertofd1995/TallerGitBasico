153069a65ffb9814976f148057fb474f883732fb
unknown <uo237068@EII-L-14-08.aulas.uniovi.es>
2015-11-23 17:38:47 +0100

    holap

--- esPrimo.py
+++ esPrimo.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 """
-Autor: X
+Editor de Spyder
+
+Este es un archivo temporal
 """
 
-
-#Nota: los primos son: Naturales, > 1, divisibles entre ellos mismo y entre 1
 def esPrimo(n):
 	"""
 	Metodo para comprobar si un numero n dado es primo o no
@@ -16,12 +17,12 @@ def esPrimo(n):
 	
 	#El rango de 2 hasta n
 	for i in range(2, n):
-		if n / i == 1:	return False
+		if  n % i == 0:	return False
 	
 	return True
 
 def esPrimoIntervalo(i, k):
-	"""
+	""""
 	Metodo que comprueba si los numeros de un intervalo son primos o no
 	params: intervalo [i ,k)
 	return: retorna una lista de duplas
@@ -29,13 +30,13 @@ def esPrimoIntervalo(i, k):
 	return [(x, esPrimo(x)) for x in range(i, k)]
 
 
-"""
-#Probando que funciona <<esPrimo(n)>>
+
+"""Probando que funciona <<esPrimo(n)>>"""
 primos = [3, 5, 7, 11, 13, 17, 19, 23, 29]
 print [(x, esPrimo(x)) for x in primos]
-"""
 
-"""
+
+
 #Probando que funciona <<esPrimoIntervalo(n)>>
 print esPrimoIntervalo(0, 1000)
-"""
+
