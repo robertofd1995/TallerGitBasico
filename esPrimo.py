ec791dc78e50c87699422ada58d149f6f13f3acb
robertofd1995 <robertofd1995@users.noreply.github.com>
2015-11-23 18:17:01 +0100

    bug fuera
    bug fuera

--- esPrimo.py
+++ esPrimo.py
@@ -16,7 +16,7 @@ def esPrimo(n):
 	
     #El rango de 2 hasta n
     for i in range(2, n):
-        if n / i == 1:	return False
+        if n % i == 0:	return False
 	
     return True
 
@@ -30,6 +30,10 @@ def esPrimoIntervalo(i, k):
 
 def cuantosDivisores(n):
     # Devolver la cantidad de divisores que tiene el número, incluyendo 1 y a sí mismo
+    contador=0
+    for i in range(1,n):
+    	if(esPrimo(n)):
+    		contador+=1
 
 
 """
